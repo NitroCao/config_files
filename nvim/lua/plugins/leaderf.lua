727a21c043b6398d5067a3e9b5ef49611c354325
Nitro Cao <jaycecao520@gmail.com>
2023-05-06 21:59:17 +0800

    feat(nvim): add Leaderf plugin in order to read linux kernel code

---
+++ nvim/lua/plugins/leaderf.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        'Yggdroot/LeaderF',
+        build = ':LeaderfInstallCExtension',
+        config = function()
+            vim.g.Lf_WindowPosition = 'popup'
+            vim.g.Lf_DefaultMode = 'FullPath'
+            vim.g.Lf_GtagsAutoGenerate = 0
+            vim.g.Lf_Gtagslabel = 'native-pygments'
+            vim.g.Lf_WildIgnore = {
+                dir = {'.svn', '.git', '.hg', 'tmp', '.build'},
+                file = {'*.o', '*.so', '*.py[co]'}
+            }
+            vim.api.nvim_set_keymap('n', '<Leader>lF', ':LeaderfFunction<CR>', { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>lf', ':LeaderfFile<CR>', { noremap = true, silent = true})
+            vim.api.nvim_set_keymap('n', '<Leader>ltr', ':<C-U><C-R>=printf("Leaderf gtags -r %s --auto-jump", expand("<cword>"))<CR><CR>', { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<Leader>ltd', ':<C-U><C-R>=printf("Leaderf gtags --auto-jump -d %s", expand("<cword>"))<CR>', { noremap = true, silent = true })
+        end
+    }
+}
