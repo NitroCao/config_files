911d927b9504c959c0e42eda84b0d5d577935b50
Nitro Cao <jaycecao520@gmail.com>
2023-06-15 09:24:48 +0800

    fix(nvim): use make to build telescope-fzf-native plugin

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
     { 'fannheyward/telescope-coc.nvim' },
     {
         'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+        build = 'make',
         lazy = false
     },
     {
