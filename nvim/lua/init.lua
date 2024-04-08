8d7e2688699597431358cc91e3ff0eaf1eb0bd74
Nitro Cao <jaycecao520@gmail.com>
2024-04-08 10:26:29 +0800

    fix(nvim): don't reset rtp paths in order to open docs of plugins using :h command

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -66,4 +66,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins')
+require('lazy').setup('plugins', {
+    performance = {
+        rtp = {
+            reset = false,
+        }
+    }
+})
