711d48881fd37db6ce756d103af8cda13727a7b1
Nitro Cao <jaycecao520@gmail.com>
2024-04-27 12:49:04 +0800

    feat: customize dashboard

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -6,10 +6,38 @@ return {
         },
         event = 'VimEnter',
         config = function()
+            local devicons = require('nvim-web-devicons')
+
             require('dashboard').setup({
+                theme = 'doom',
                 config = {
-                    week_header = {
-                        enable = true
+                    header = {
+'██████╗ ███████╗    ███╗   ███╗██╗   ██╗███████╗███████╗██╗     ███████╗',
+'██╔══██╗██╔════╝    ████╗ ████║╚██╗ ██╔╝██╔════╝██╔════╝██║     ██╔════╝',
+'██████╔╝█████╗      ██╔████╔██║ ╚████╔╝ ███████╗█████╗  ██║     █████╗  ',
+'██╔══██╗██╔══╝      ██║╚██╔╝██║  ╚██╔╝  ╚════██║██╔══╝  ██║     ██╔══╝  ',
+'██████╔╝███████╗    ██║ ╚═╝ ██║   ██║   ███████║███████╗███████╗██║     ',
+'╚═════╝ ╚══════╝    ╚═╝     ╚═╝   ╚═╝   ╚══════╝╚══════╝╚══════╝╚═╝     ',
+                    },
+                    center = {
+                        {
+                            icon = '󰏓 ',
+                            desc = 'Projects',
+                            key = 'p',
+                            action = 'Telescope project',
+                        },
+                        {
+                            icon = devicons.get_icon_by_filetype('vim', {}) .. ' ',
+                            desc = 'Neovim Config',
+                            key = ',',
+                            action = ':chdir ~/.dotfiles/nvim | edit lua/init.lua',
+                        },
+                        {
+                            icon = '󰒲 ',
+                            desc = 'Lazy',
+                            key = 'l',
+                            action = ':Lazy',
+                        },
                     }
                 }
             })
