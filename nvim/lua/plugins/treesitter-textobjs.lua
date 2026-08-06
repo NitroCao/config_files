17b19ddfc79b73b22e3ad24fae8ae972b7ea832a
Nitro Cao <jaycecao520@gmail.com>
2026-08-07 00:56:54 +0800

    fix(nvim): adapt nvim-treesitter to rewritten main branch
    - use new setup API (highlight/indent/incremental_selection removed)
    - enable highlighting via core vim.treesitter.start()
    - fix parser install filter and drop unsupported jsonc parser
    - update textobjects to new setup API and repeatable_move module

--- nvim/lua/plugins/treesitter-textobjs.lua
+++ nvim/lua/plugins/treesitter-textobjs.lua
@@ -1,59 +1,68 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter-textobjects',
+        branch = 'main',
         priority = 0,
         lazy = false,
         dependencies = {
             'nvim-treesitter/nvim-treesitter',
         },
         config = function ()
-            local ts_repeat_move = require('nvim-treesitter.textobjects.repeatable_move')
+            local ts_repeat_move = require('nvim-treesitter-textobjects.repeatable_move')
 
             vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
             vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-            vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-            vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-            vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-            vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+            -- 新版只提供 builtin_f_expr 等函数，且必须带 { expr = true }
+            vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+            vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+            vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+            vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
 
-            require('nvim-treesitter.configs').setup{
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["ic"] = "@conditional.inner",
-                            ["ac"] = "@conditional.outer",
-                            ["ip"] = "@parameter.inner",
-                            ["ap"] = "@parameter.outer",
-                        }
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true,
-                        goto_next_start = {
-                            ["]f"] = "@function.outer",
-                            ["]c"] = "@conditional.outer",
-                            ["]p"] = "@parameter.outer",
-                        },
-                        goto_next_end = {
-                            ["]F"] = "@function.outer",
-                            ["]C"] = "@conditional.outer",
-                            ["]P"] = "@parameter.outer",
-                        },
-                        goto_previous_start = {
-                            ["[f"] = "@function.outer",
-                            ["[c"] = "@conditional.outer",
-                            ["[p"] = "@parameter.outer",
-                        },
-                        goto_previous_end = {
-                            ["[F"] = "@function.outer",
-                            ["[C"] = "@conditional.outer",
-                            ["[P"] = "@parameter.outer",
-                        }
-                    }
-                }
+            require('nvim-treesitter-textobjects').setup {
+                select = {
+                    -- 新版不再支持 keymaps，键位需要自己用 vim.keymap.set 绑定
+                    lookahead = true,
+                },
+                move = {
+                    set_jumps = true,
+                },
             }
+
+            -- select 文本对象
+            local select = require('nvim-treesitter-textobjects.select')
+            vim.keymap.set({ "x", "o" }, "ic", function()
+                select.select_textobject("@conditional.inner", "textobjects")
+            end)
+            vim.keymap.set({ "x", "o" }, "ac", function()
+                select.select_textobject("@conditional.outer", "textobjects")
+            end)
+            vim.keymap.set({ "x", "o" }, "ip", function()
+                select.select_textobject("@parameter.inner", "textobjects")
+            end)
+            vim.keymap.set({ "x", "o" }, "ap", function()
+                select.select_textobject("@parameter.outer", "textobjects")
+            end)
+
+            -- move 跳转
+            local move = require('nvim-treesitter-textobjects.move')
+            local function bind_move(mapping, fn, query)
+                vim.keymap.set({ "n", "x", "o" }, mapping, function()
+                    fn(query, "textobjects")
+                end)
+            end
+
+            bind_move("]f", move.goto_next_start, "@function.outer")
+            bind_move("]c", move.goto_next_start, "@conditional.outer")
+            bind_move("]p", move.goto_next_start, "@parameter.outer")
+            bind_move("]F", move.goto_next_end, "@function.outer")
+            bind_move("]C", move.goto_next_end, "@conditional.outer")
+            bind_move("]P", move.goto_next_end, "@parameter.outer")
+            bind_move("[f", move.goto_previous_start, "@function.outer")
+            bind_move("[c", move.goto_previous_start, "@conditional.outer")
+            bind_move("[p", move.goto_previous_start, "@parameter.outer")
+            bind_move("[F", move.goto_previous_end, "@function.outer")
+            bind_move("[C", move.goto_previous_end, "@conditional.outer")
+            bind_move("[P", move.goto_previous_end, "@parameter.outer")
         end
     }
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,49 +1,62 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
+        branch = 'main',
         build = ':TSUpdate',
         config = function()
-            require('nvim-treesitter.configs').setup{
-                ensure_installed = {
-                    'bash',
-                    'c',
-                    'cmake',
-                    'cpp',
-                    'dockerfile',
-                    'go',
-                    'gomod',
-                    'java',
-                    'javascript',
-                    'json',
-                    'json5',
-                    'jsonc',
-                    'lua',
-                    'make',
-                    'markdown',
-                    'markdown_inline',
-                    'perl',
-                    'php',
-                    'python',
-                    'toml',
-                    'yaml'
-                },
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false
-                },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = '<CR>',
-                        node_incremental = '<CR>',
-                        node_decremental = '<BS>',
-                        scope_incremental = '<TAB>'
-                    }
-                },
-                indent = {
-                    enable = false
-                }
+            -- 新版 nvim-treesitter（main 分支）只支持 install_dir 这一个 setup 选项，
+            -- highlight / incremental_selection / indent 等旧配置项已被移除。
+            require('nvim-treesitter').setup {
+                -- install_dir = vim.fn.stdpath('data') .. '/site', -- 默认值，可省略
             }
+
+            local ensure_installed = {
+                'bash',
+                'c',
+                'cmake',
+                'cpp',
+                'dockerfile',
+                'go',
+                'gomod',
+                'html',
+                'java',
+                'javascript',
+                'json',
+                'json5',
+                'lua',
+                'make',
+                'markdown',
+                'markdown_inline',
+                'perl',
+                'php',
+                'python',
+                'regex',
+                'toml',
+                'yaml',
+                -- 高亮 vim 配置 / 帮助文档 / query 文件
+                'vim',
+                'vimdoc',
+                'query',
+            }
+
+            local installed = require('nvim-treesitter').get_installed()
+            local parsers_to_install = vim.iter(ensure_installed)
+                :filter(function(parser)
+                    return not vim.tbl_contains(installed, parser)
+                end)
+                :totable()
+
+            if #parsers_to_install > 0 then
+                -- 异步安装缺失的 parser；之后可用 :TSInstall <lang> / :TSUpdate 手动管理
+                require('nvim-treesitter').install(parsers_to_install)
+            end
+
+            -- 新版不再通过 setup 开启高亮，改为调用 Neovim 核心的 treesitter 高亮
+            vim.api.nvim_create_autocmd('FileType', {
+                callback = function()
+                    pcall(vim.treesitter.start)
+                end,
+            })
         end
     }
 }
